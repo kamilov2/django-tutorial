56b6e5ecfd36da984ede57fd636dc757c354f628
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-05 15:19:57 +0500

    update

--- examplesite/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Club,Leaguage,Player
-# Register your models here.
-
-admin.site.register(Club)
-admin.site.register(Leaguage)
-admin.site.register(Player)
--- examplesite/users/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-class CustomUserRegisterForm(UserCreationForm):
-    
-    class Meta:
-        model = User
-        fields = ["username","first_name","email","password1","password2"]
