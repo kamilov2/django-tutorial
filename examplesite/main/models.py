b31a131a8b77e15615b9955adb0289eeae97cae3
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-07 15:39:10 +0500

    update

--- examplesite/main/models.py
+++ examplesite/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(verbose_name="Kurs nomi",max_length=250)
+    duration = models.CharField("Davomiyligi", max_length=250)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+DAYS = (
+    ("dchj","Du-Chor-Ju"),
+    ("spsh","Se-Pa-Sha"),
+)
+class Group(models.Model):
+    name = models.CharField(verbose_name="Guruh nomi",max_length=250)
+    teach_days = models.CharField(max_length=250, choices=DAYS)
+    price = models.PositiveBigIntegerField(default=0)
+    room = models.PositiveSmallIntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.name}"
--- examplesite/main/views.py
+++ examplesite/main/views.py
@@ -1,5 +1,12 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, "index.html")
+    data = {
+        "sitename":"examplesite",
+        "frontent":"html and css",
+        "backend":"Python Django",
+        "now":datetime.datetime.now()
+    }
+    return render(request, "index.html", context=data)
