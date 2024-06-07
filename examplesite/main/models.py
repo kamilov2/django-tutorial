8e4fed9553bebb2681f427afc2710e8cd30f0c46
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-07 15:51:14 +0500

    Update models.py

--- examplesite/main/models.py
+++ examplesite/main/models.py
@@ -12,6 +12,11 @@ DAYS = (
     ("spsh","Se-Pa-Sha"),
 )
 class Group(models.Model):
+    course = models.ForeignKey( # birga ko'p ulash
+        to=Course, 
+        on_delete=models.PROTECT, # agar kurs o'chib ketsa guruhlar qoladi
+        related_name="groups", # murojaat qilish uchun nom 
+        null=True)
     name = models.CharField(verbose_name="Guruh nomi",max_length=250)
     teach_days = models.CharField(max_length=250, choices=DAYS)
     price = models.PositiveBigIntegerField(default=0)
@@ -19,3 +24,17 @@ class Group(models.Model):
     
     def __str__(self):
         return f"{self.name}"
+
+class Student(models.Model):
+    group = models.ForeignKey( # birga ko'p ulash
+        to=Group, 
+        on_delete=models.PROTECT, # agar kurs o'chib ketsa guruhlar qoladi
+        related_name="students", # murojaat qilish uchun nom 
+        null=True)
+    first_name = models.CharField("Ismi", max_length=250)
+    last_name = models.CharField("Familya", max_length=250)
+    phone = models.CharField("Telefon", max_length=250)
+    address = models.CharField("Manzil", max_length=250)
+
+    def __str__(self):
+        return f"{self.first_name}"
