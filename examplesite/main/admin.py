dc160017a3f1addbee043169bdb034556d7c5226
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-07 16:00:25 +0500

    Update admin.py

--- examplesite/main/admin.py
+++ examplesite/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Course,Group,Student
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Group)
+admin.site.register(Student)
+
+
