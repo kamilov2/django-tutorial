4cb737f95ae0603e6843c5427c6489c98cee5ae9
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-10 16:30:59 +0500

    update

--- examplesite/main/views.py
+++ examplesite/main/views.py
@@ -12,3 +12,9 @@ def home_page(request):
     }
     # print(dir(Course.objects.all()))
     return render(request, "index.html", context=data)
+
+def course_detail(request,course_id):
+    context = {
+        "object":Course.objects.get(id=course_id)
+    }
+    return render(request, "course-detail.html", context)
